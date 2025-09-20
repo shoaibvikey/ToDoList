c1ce13ce304fbf18542e2d18cb8152dcaf284766
shoaibvikey <shoaibakhtarvikey@gmail.com>
2025-09-20 12:29:09 +0530

    implemented dark mode.

--- ToDoList/Views/NoItemsView.swift
+++ ToDoList/Views/NoItemsView.swift
@@ -13,20 +13,29 @@ struct NoItemsView: View {
     var body: some View {
         ScrollView{
             VStack(spacing: 10){
+                Image(systemName: "target")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .foregroundStyle(Color.gray)
                 Text("There are no items!")
                     .font(.title)
                     .fontWeight(.semibold)
+                    .foregroundStyle(Color.gray)
                 Text("Are you a productive person? I think you should click the add button and add some tasks.")
-                NavigationLink(destination: AddView()) {
-                    Text("Add something")
-                        .foregroundStyle(Color.white)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 50)
-                        .background(Color.blue)
-                        .cornerRadius(20)
-                }
+                    .font(.caption)
+                    .foregroundStyle(Color.gray)
+//                NavigationLink(destination: AddView()) {
+//                    Text("Add something")
+//                        .foregroundStyle(Color.white)
+//                        .frame(maxWidth: .infinity)
+//                        .frame(height: 50)
+//                        .background(Color.gray)
+//                        .cornerRadius(20)
+//                }
+                
             }
             .padding(40)
+            .padding(.top, 100)
             .multilineTextAlignment(.center)
             
         }
